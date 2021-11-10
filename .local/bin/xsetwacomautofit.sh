33a6785f87f93b3499fed7caa680a8e039489f28
Ryan N Johnson <ryan@ryannjohnson.com>
2021-11-10 05:08:16 +0000

    Add automatic Wacom config on plug in

---
+++ .local/bin/xsetwacomautofit.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCREEN_DIMENSIONS=$(xrandr | grep '*' | awk '{print $1}')
+TARGET_WIDTH=$(echo $SCREEN_DIMENSIONS | awk -F'x' '{print $1}')
+TARGET_HEIGHT=$(echo $SCREEN_DIMENSIONS | awk -F'x' '{print $2}')
+
+DEVICE_ID=$(
+        xsetwacom --list devices \
+                | grep 'type: STYLUS' \
+                | awk -F"\t" '{print $2}' \
+                | awk -F": " '{print $2}'
+)
+
+xsetwacom --set "$DEVICE_ID" ResetArea
+AREA=$(xsetwacom --get "$DEVICE_ID" Area)
+TABLET_WIDTH=$(echo $AREA | awk '{print $3}')
+TABLET_HEIGHT=$(echo $AREA | awk '{print $4}')
+
+TABLET_ASPECT=$(echo "$TABLET_WIDTH/$TABLET_HEIGHT" | bc -l)
+TARGET_ASPECT=$(echo "$TARGET_WIDTH/$TARGET_HEIGHT" | bc -l)
+TABLET_IS_WIDER=$(echo "$TABLET_ASPECT>$TARGET_ASPECT" | bc -l)
+if [[ "$TABLET_IS_WIDER" == "1" ]]; then
+	NEW_WIDTH=$(printf '%0.f' $(echo "$TABLET_HEIGHT*$TARGET_ASPECT" | bc -l))
+	xsetwacom --set "$DEVICE_ID" Area 0 0 $NEW_WIDTH $TABLET_HEIGHT
+else
+	NEW_HEIGHT=$(printf '%0.f' $(echo "$TABLET_WIDTH/$TARGET_ASPECT" | bc -l))
+	xsetwacom --set "$DEVICE_ID" Area 0 0 $TABLET_WIDTH $NEW_HEIGHT
+fi
